e896adf4a7bec524d26d86116c94ef0779ed9c7d
campusbazeofficial <campusbazeofficial@gmail.com>
2026-04-29 20:24:00 +0100

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 // src/api/api.js
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://campusbasebackend.onrender.com'
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://campusbazebackend.onrender.com'
 
 const api = axios.create({
   baseURL: BASE_URL,
